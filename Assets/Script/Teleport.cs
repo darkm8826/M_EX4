6cbbf19215baa89bb4987fc5b7a2a8d905cdc4e0
darkm8826 <darkm8826@gmail.com>
2022-10-23 14:33:31 +0800

    新增雙向傳送點(兔子洞)

---
+++ Assets/Script/Teleport.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform TeleportSpot;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            other.gameObject.transform.position = TeleportSpot.gameObject.transform.position;
+            TeleportSpot.gameObject.SetActive(false);
+        }
+    }
+}
